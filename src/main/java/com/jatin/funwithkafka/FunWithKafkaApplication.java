018f79305bc8de167a11554697221d2133d24a28
Jatin Bansal <88074939+jatin-bansal-21@users.noreply.github.com>
2022-09-08 20:20:54 +0530

    initial commit

---
+++ src/main/java/com/jatin/funwithkafka/FunWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jatin.funwithkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunWithKafkaApplication.class, args);
+    }
+
+}
